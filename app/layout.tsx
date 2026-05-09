769a372399a21756235c22c1214c3157a5274d3b
ryumy97 <inha.ryu.97@gmail.com>
2026-05-09 22:58:12 +1200

    Smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 			lang="en"
 			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
 		>
-			<body className="min-h-screen flex flex-col">
+			<body className="min-h-screen bg-white">
 				{children}
 				<PageTunnelOut />
 			</body>
