3ff3eda6aab55c93c331158435fe39cf1336ca98
ryumy97 <inha.ryu.97@gmail.com>
2026-05-14 10:02:32 +1200

    animate gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { PageTunnelOut } from "@/components/page-tunnel";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Preloader } from "@/components/preloader";
 import Loader from "@/components/loader";
 
 const geistSans = Geist({
@@ -33,7 +32,6 @@ export default function RootLayout({
 			<body className="min-h-screen bg-white overflow-hidden">
 				{children}
 				<PageTunnelOut />
-				<Preloader />
 				<Loader />
 			</body>
 		</html>
