4c21fd4d26b5dfc751e78fd09f49f55ab306a619
ryumy97 <inha.ryu.97@gmail.com>
2026-05-14 23:40:58 +1200

    Transition

--- app/(experience)/about/page.tsx
+++ app/(experience)/about/page.tsx
@@ -10,7 +10,6 @@ import SmoothScroll from "@/components/smooth-scroll";
 import { WebGLRippleCanvas } from "@/components/webgl/webgl-ripple-canvas";
 import { useIntroStore } from "@/stores/intro";
 import { Canvas } from "@react-three/fiber";
-import { cubicBezier, motion } from "motion/react";
 import Link from "next/link";
 
 export default function Page2() {
@@ -18,59 +17,24 @@ export default function Page2() {
 
 	return (
 		<PageTunnelIn>
-			<motion.div
-				key="page2"
-				className="absolute inset-0 bg-background h-screen w-full overflow-hidden"
-				initial={
-					state === "end"
-						? {
-								y: "100%",
-								borderTopLeftRadius: "5vw",
-								borderTopRightRadius: "5vw",
-							}
-						: { y: 0, borderTopLeftRadius: "0vw", borderTopRightRadius: "0vw" }
-				}
-				animate={{
-					y: 0,
-					borderTopLeftRadius: "0vw",
-					borderTopRightRadius: "0vw",
-					transition: {
-						delay: 0.2,
-						duration: 0.8,
-						ease: cubicBezier(0.3, 0, 0, 1),
-					},
-				}}
-				exit={{
-					y: "0%",
-					borderTopLeftRadius: "5vw",
-					borderTopRightRadius: "5vw",
-					scale: 0.9,
-					opacity: 0.9,
-					transition: {
-						duration: 0.8,
-						ease: cubicBezier(0.3, 0, 0, 1),
-					},
-				}}
-			>
-				<WebGLRippleCanvas className="fixed inset-0 h-full w-full" />
-				<SmoothScroll horizontal>
-					<main className="flex min-h-screen w-max items-center gap-4 px-8">
-						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
-						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
-						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
-						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
-						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
-						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
-						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
-						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
-						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
-						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
-						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
-						<Link href="/">Home</Link>
-					</main>
-					<Canvas className="fixed inset-0"></Canvas>
-				</SmoothScroll>
-			</motion.div>
+			<WebGLRippleCanvas className="fixed inset-0 h-full w-full" />
+			<SmoothScroll horizontal>
+				<main className="flex min-h-screen w-max items-center gap-4 px-8">
+					<div className="h-96 w-lg shrink-0 bg-red-500"></div>
+					<div className="h-96 w-lg shrink-0 bg-red-500"></div>
+					<div className="h-96 w-lg shrink-0 bg-red-500"></div>
+					<div className="h-96 w-lg shrink-0 bg-red-500"></div>
+					<div className="h-96 w-lg shrink-0 bg-red-500"></div>
+					<div className="h-96 w-lg shrink-0 bg-red-500"></div>
+					<div className="h-96 w-lg shrink-0 bg-red-500"></div>
+					<div className="h-96 w-lg shrink-0 bg-red-500"></div>
+					<div className="h-96 w-lg shrink-0 bg-red-500"></div>
+					<div className="h-96 w-lg shrink-0 bg-red-500"></div>
+					<div className="h-96 w-lg shrink-0 bg-red-500"></div>
+					<Link href="/">Home</Link>
+				</main>
+				<Canvas className="fixed inset-0"></Canvas>
+			</SmoothScroll>
 		</PageTunnelIn>
 	);
 }
--- components/page-tunnel.tsx
+++ components/page-tunnel.tsx
@@ -1,11 +1,50 @@
 "use client";
 
+import { cubicBezier, motion } from "motion/react";
 import tunnel from "./ui/tunnel";
+import { useIntroStore } from "@/stores/intro";
+import { usePathname } from "next/navigation";
 
 const PageTunnel = tunnel();
 
 export const PageTunnelIn = ({ children }: { children: React.ReactNode }) => {
-	return <PageTunnel.In>{children}</PageTunnel.In>;
+	const { state } = useIntroStore();
+
+	const pathname = usePathname();
+
+	return (
+		<PageTunnel.In>
+			<motion.div
+				key={pathname}
+				className="absolute inset-0 bg-background h-screen w-full overflow-hidden origin-bottom"
+				initial={
+					state === "end"
+						? {
+								y: "100%",
+							}
+						: { y: 0 }
+				}
+				animate={{
+					y: 0,
+					transition: {
+						delay: 0.2,
+						duration: 0.8,
+						ease: cubicBezier(0.3, 0, 0, 1),
+					},
+				}}
+				exit={{
+					y: "10%",
+					opacity: 0.9,
+					transition: {
+						duration: 0.8,
+						ease: cubicBezier(0.3, 0, 0, 1),
+					},
+				}}
+			>
+				{children}
+			</motion.div>
+		</PageTunnel.In>
+	);
 };
 
 export const PageTunnelOut = () => {
