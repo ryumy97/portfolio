cc8a979da529f3bd86cf71c90663105159ed2408
ryumy97 <inha.ryu.97@gmail.com>
2026-05-14 23:23:15 +1200

    added dev links

---
+++ app/(dev)/canvas/ripple/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Grid } from "@/components/ui/grid";
+import { WebGLRippleCanvas } from "@/components/webgl/webgl-ripple-canvas";
+import Link from "next/link";
+
+export default function Home() {
+	return (
+		<div className="relative w-full h-full">
+			<WebGLRippleCanvas className="fixed inset-0 h-full w-full" />
+			<Grid className="fixed inset-0 h-full w-full">
+				<div className="col-start-4 col-end-8 flex items-center justify-center">
+					<h1 className="relative text-foreground text-[10vw] font-heading font-bold transition-all duration-300 group">
+						Ripple
+						<div className="absolute bottom-[8%] left-0 bg-foreground w-0 group-hover:w-full transition-all duration-300 h-1" />
+					</h1>
+				</div>
+			</Grid>
+			<Grid className="absolute top-0 left-0" asChild>
+				<header className="col-span-full p-2">
+					<Button
+						variant="nav"
+						size={"text"}
+						className="col-start-1 justify-start"
+						asChild
+					>
+						<Link href="/">Back</Link>
+					</Button>
+				</header>
+			</Grid>
+		</div>
+	);
+}
--- app/(experience)/about/page.tsx
+++ app/(experience)/about/page.tsx
@@ -7,21 +7,52 @@
 
 import { PageTunnelIn } from "@/components/page-tunnel";
 import SmoothScroll from "@/components/smooth-scroll";
+import { WebGLRippleCanvas } from "@/components/webgl/webgl-ripple-canvas";
+import { useIntroStore } from "@/stores/intro";
 import { Canvas } from "@react-three/fiber";
-import { motion } from "motion/react";
+import { cubicBezier, motion } from "motion/react";
 import Link from "next/link";
 
 export default function Page2() {
+	const { state } = useIntroStore();
+
 	return (
 		<PageTunnelIn>
 			<motion.div
 				key="page2"
-				className="relative bg-background min-h-screen"
-				initial={{ opacity: 0, y: 100 }}
-				animate={{ opacity: 1, y: 0 }}
-				exit={{ opacity: 0, y: -100 }}
-				transition={{ duration: 0.5 }}
+				className="absolute inset-0 bg-background h-screen w-full overflow-hidden"
+				initial={
+					state === "end"
+						? {
+								y: "100%",
+								borderTopLeftRadius: "5vw",
+								borderTopRightRadius: "5vw",
+							}
+						: { y: 0, borderTopLeftRadius: "0vw", borderTopRightRadius: "0vw" }
+				}
+				animate={{
+					y: 0,
+					borderTopLeftRadius: "0vw",
+					borderTopRightRadius: "0vw",
+					transition: {
+						delay: 0.2,
+						duration: 0.8,
+						ease: cubicBezier(0.3, 0, 0, 1),
+					},
+				}}
+				exit={{
+					y: "0%",
+					borderTopLeftRadius: "5vw",
+					borderTopRightRadius: "5vw",
+					scale: 0.9,
+					opacity: 0.9,
+					transition: {
+						duration: 0.8,
+						ease: cubicBezier(0.3, 0, 0, 1),
+					},
+				}}
 			>
+				<WebGLRippleCanvas className="fixed inset-0 h-full w-full" />
 				<SmoothScroll horizontal>
 					<main className="flex min-h-screen w-max items-center gap-4 px-8">
 						<div className="h-96 w-lg shrink-0 bg-red-500"></div>
