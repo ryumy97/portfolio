185fe2702ff74a6394fbc4991066f457c80a8002
ryumy97 <inha.ryu.97@gmail.com>
2026-05-21 16:40:26 +1200

    add more project pages

--- app/(experience)/home/projects.tsx
+++ app/(experience)/home/projects.tsx
@@ -1,55 +1,55 @@
 "use client";
 
+import { MotionImage } from "@/components/motion-image";
 import { PointerEventHandler } from "@/components/pointer";
-import { useLenis } from "lenis/react";
-import { cubicBezier, transform, useMotionValue } from "motion/react";
+import { ArrowRightIcon } from "lucide-react";
+import { motion, transform, useMotionValue } from "motion/react";
 import Link from "next/link";
 import { useRef } from "react";
 import { useScrollEvent } from "../../../components/smooth-scroll";
 import { Grid, SubGrid } from "../../../components/ui/grid";
-import { MotionImage } from "@/components/motion-image";
 import Kiwi from "./assets/kiwi.png";
 import Typography from "./assets/typography.png";
+import Image from "next/image";
+import AimHigh from "./assets/aimhigh.png";
+import Reflct from "./assets/reflct.png";
+import LiveOcean from "./assets/liveocean.png";
+import Fola from "./assets/fola.png";
 
 const About = () => {
-	const lenis = useLenis();
-	const ref = useRef<HTMLDivElement>(null);
+	const containerRef = useRef<HTMLDivElement>(null);
+	const gridRef = useRef<HTMLDivElement>(null);
 
-	const width = useMotionValue("50%");
-	const opacity = useMotionValue(1);
-
-	const pixelSize = useMotionValue(64);
-	const radius = useMotionValue(1);
+	const x = useMotionValue("0%");
 
 	useScrollEvent(() => {
-		const top = ref.current?.getBoundingClientRect().top ?? 0;
-		const progress = (window.innerHeight - top) / window.innerHeight;
+		const rect = containerRef.current?.getBoundingClientRect();
 
-		// width
-		const w = transform(progress, [0, 1], [50, 100], {
-			clamp: true,
-			ease: cubicBezier(0.3, 0, 0, 1),
-		});
-		width.set(`${w}%`);
+		if (!rect) return;
 
-		// opacity
-		const op = transform(progress, [0, 1], [1, 0], {
-			clamp: true,
-			ease: cubicBezier(0.3, 0, 0, 1),
-		});
+		const progressValue = transform(
+			window.innerHeight - rect.top,
+			[0, rect.height + window.innerHeight],
+			[0, 1],
+			{
+				clamp: true,
+			},
+		);
 
-		opacity.set(op);
+		const gridWidth = gridRef.current?.clientWidth;
+		const windowWidth = window.innerWidth;
 
-		// pixel size
-		const px = transform(progress, [0, 1], [64, 24], {
-			clamp: true,
-			ease: cubicBezier(0.3, 0, 0, 1),
-		});
+		if (!gridWidth || !windowWidth) return;
 
-		pixelSize.set(px);
-
-		// radius
-		radius.set(1);
+		const xValue = transform(
+			progressValue,
+			[0, 1],
+			[0, (windowWidth - gridWidth) / gridWidth],
+			{
+				clamp: true,
+			},
+		);
+		x.set(`${xValue * 100}%`);
 	});
 
 	return (
@@ -57,33 +57,62 @@ const About = () => {
 			<p className="row-start-1 col-start-7 text-[min(max(10vw,18px),32px)]">
 				I develop websites
 			</p>
-			<div className="w-full col-span-full aspect-video flex items-center justify-center relative">
-				<Grid className="absolute inset-0">
-					<MotionImage
-						className="col-start-2 row-start-2"
-						src={Kiwi}
-						alt="Kiwi"
-					/>
-					<MotionImage
-						className="col-start-4 row-start-1"
-						src={Typography}
-						alt="Typography"
-					/>
-				</Grid>
+			<div className="col-start-4 row-start-2 flex items-center justify-start mt-6">
 				<PointerEventHandler asChild type="underline">
 					<Link
 						href="/projects"
-						className="text-[8vw] flex items-center justify-start gap-2 font-heading font-bold relative z-10"
+						className="text-[min(max(8vw,16px),24px)] flex items-center justify-start gap-2"
 					>
 						Projects
+						<ArrowRightIcon
+							className="w-[min(max(8vw,16px),24px)]"
+							strokeWidth={1.5}
+						/>
 					</Link>
 				</PointerEventHandler>
-				{/* <Image className="col-start-2 row-start-2" src={Kiwi} alt="Kiwi" />
-				<Image
-					className=" col-start-3 row-start-3"
-					src={Typography}
-					alt="Typography"
-				/> */}
+			</div>
+			<div
+				className="w-full col-span-full relative overflow-hidden -mx-2"
+				ref={containerRef}
+			>
+				<Grid
+					className="relative h-full py-[5vw] w-[120%] max-w-none px-2"
+					asChild
+				>
+					<motion.div
+						ref={gridRef}
+						style={{
+							x,
+						}}
+					>
+						<Image className="col-start-2 row-start-3" src={Kiwi} alt="Kiwi" />
+						<Image
+							className="col-start-4 row-start-1"
+							src={Typography}
+							alt="Typography"
+						/>
+						<Image
+							className="col-start-6 col-span-2 row-start-2"
+							src={AimHigh}
+							alt="AimHigh"
+						/>
+						<Image
+							className="col-start-8 col-span-2 row-start-4"
+							src={Reflct}
+							alt="Reflect"
+						/>
+						<Image
+							className="col-start-4 col-span-2 row-start-3"
+							src={LiveOcean}
+							alt="LiveOcean"
+						/>
+						<Image
+							className="col-start-7 col-span-2 row-start-1"
+							src={Fola}
+							alt="Fola"
+						/>
+					</motion.div>
+				</Grid>
 			</div>
 		</SubGrid>
 	);
