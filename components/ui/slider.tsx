ac07b2f1c64b0e8ad4556ba922ee31ae733c6bb5
ryumy97 <inha.ryu.97@gmail.com>
2026-06-05 19:17:42 +1200

    updated husky

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { Slider as SliderPrimitive } from "radix-ui";
 import * as React from "react";
+import { cn } from "@/lib/utils";
 import { usePointerEvent } from "../pointer";
 
 function Slider({
