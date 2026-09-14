c733ade5c6e36ecabfffaffafb1cf17ef2c7e7cf
ryumy97 <inha.ryu.97@gmail.com>
2026-09-14 18:42:16 +1200

    remove canvas

--- app/home/hero.tsx
+++ app/home/hero.tsx
@@ -1,13 +1,10 @@
 import { Grid } from "@/components/ui/grid";
 import { PageDescription, Title } from "@/components/ui/typography";
-import HeroCanvas from "./hero-canvas";
 
 const Hero = () => {
   return (
     <Grid className="h-svh w-full px-2 grid-rows-[1fr_auto]">
-      <div className="relative col-start-2 col-end-9 md:col-end-7 min-h-0 overflow-hidden">
-        <HeroCanvas />
-      </div>
+      <div className="relative col-start-2 col-end-9 md:col-end-7 min-h-0 overflow-hidden"></div>
       <div className="col-start-2 col-end-9 md:col-end-7 py-2 text-ink mt-4">
         <Title>In Ha Ryu</Title>
         <PageDescription className="mt-4">
